87340c633c7a01d916988865f5741c1924ca1c86
GagakIrengg <noel.535210103@stu.untar.ac.id>
2023-05-06 00:23:51 +0700

    editpost

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/forgot_page.dart';
 import 'package:flutter_auth/Screens/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_auth/main-page/main_page.dart';
 import 'Screens/auth_page.dart';
 import 'firebase_options.dart';
+import 'package:flutter_auth/main-page/main_page.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
-} 
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
